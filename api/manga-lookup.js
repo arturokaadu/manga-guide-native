9a3963fa27326697eb5723641c2ff37ee6d2bb1c
arturokaadu <arturokaadu@gmail.com>
2026-01-14 18:31:56 -0300

    fix(sec): remove hardcoded api key, use env var

--- api/manga-lookup.js
+++ api/manga-lookup.js
@@ -3,7 +3,7 @@
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyCd96kOFZVqM9zkChRrd3peQ00YiMrl0Xo");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 module.exports = async (req, res) => {
     // CORS - allow frontend domain
